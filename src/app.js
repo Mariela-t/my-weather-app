b7db6af7fcd2a992b5214900e4e311ed796a4aec
Mariela-t <91850798+Mariela-t@users.noreply.github.com>
2021-12-17 15:20:35 -0800

    inserted forecast HTML into JS

--- src/app.js
+++ src/app.js
@@ -25,6 +25,33 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function showForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Sun", "Mon", "Tue"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      ` <div class="col-2">
+        <div class="forecast-day">${day}</div>
+                <img
+                  src="https://openweathermap.org/img/wn/01n@2x.png"
+                  alt=""
+                  width="42"
+                />
+          <div class="forecast-temp">
+              <span class="forecast-temp-max"> 18 </span>
+              <span class="forecast-temp-min">12</span>
+        </div>
+      </div>
+            `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function showTemperature(response) {
   document.querySelector("#current-city").innerHTML = response.data.name;
   celsiusTemperature = Math.round(response.data.main.temp);
@@ -88,3 +115,4 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
 search("New York");
+showForecast();
