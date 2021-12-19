ec6cfc62aa5e26b4ddc770ec5a6ccee8bcb1f5ff
Mariela-t <91850798+Mariela-t@users.noreply.github.com>
2021-12-18 23:42:35 -0800

    Displayed the forecast data

--- src/app.js
+++ src/app.js
@@ -25,28 +25,46 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function showForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Sun", "Mon", "Tue"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      ` <div class="col-2">
-        <div class="forecast-day">${day}</div>
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        ` <div class="col-2">
+        <div class="forecast-day">${formatDay(forecastDay.dt)}</div>
+       
                 <img
-                  src="https://openweathermap.org/img/wn/01n@2x.png"
+                  src="https://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
                   alt=""
                   width="42"
                 />
           <div class="forecast-temp">
-              <span class="forecast-temp-max"> 18 </span> |
-              <span class="forecast-temp-min">12</span>
+              <span class="forecast-temp-max"> ${Math.round(
+                forecastDay.temp.max
+              )}° </span> |
+              <span class="forecast-temp-min">${Math.round(
+                forecastDay.temp.min
+              )}°</span>
         </div>
       </div>
             `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -126,4 +144,3 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
 search("New York");
-showForecast();
