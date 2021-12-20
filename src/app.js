70327e8833c1442a8170b76610ee0081af1e3203
Mariela-t <91850798+Mariela-t@users.noreply.github.com>
2021-12-19 23:12:17 -0800

    Added CSS to search button and removed console

--- src/app.js
+++ src/app.js
@@ -72,10 +72,9 @@ function showForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "9f89053bd4f1be266f217aa66c188ea7";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
+
   axios.get(apiUrl).then(showForecast);
 }
 
